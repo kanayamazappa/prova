67d7a3b8bd203c5cd327552265189f62e21ead8b
kanayamazappa <kanayama.zappa@gmail.com>
2016-02-26 16:45:42 -0300

    Delete serializer.py

--- serializer.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from rest_framework import serializers
-from models import Person
-
-class PersonSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Person
-        fields = ('facebookId', 'name', 'link', 'gender')
