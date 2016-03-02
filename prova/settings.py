4e94fcf6f8b65e4c7c261f56e2e37cd0be5a5dca
kanayamazappa <kanayama.zappa@gmail.com>
2016-03-02 08:58:13 -0300

    Update settings.py

--- prova/settings.py
+++ prova/settings.py
@@ -150,7 +150,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        #'rest_framework.permissions.IsAuthenticated',
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'TEST_REQUEST_RENDERER_CLASSES': (
